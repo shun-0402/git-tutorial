0e4a3e845445fafa113583c4ed1b8e03576ba62f
Shun Takamoto <shirogamersun@gmail.com>
2019-10-07 09:33:12 +0900

    finish fizzbuzz program

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,13 @@
 
 for i in 1..100
  if i%3 == 0 && i % 5 == 0 then 
-   puts i
+   # puts i
    print ("FizzBuzz\n ")
+ elsif i%3 ==0 then
+   print ("Fizz\n")
+ elsif i%5 ==0 then
+   print ("Buzz\n")
+ else 
+   puts "#{i}\n"
  end
 end
